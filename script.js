8afb96d76be15153562200ac439d4b1dd14c0859
na1969na <nakmtn07@gmail.com>
2024-11-27 11:26:55 -0800

    Done exercise

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 $(function() {
-  // your code here
-})
+  $(".accordion-header").click(function() {
+    $(".accordion-content").not($(this).next()).slideUp();
+        $(this).next(".accordion-content").slideDown();
+  });
+
+  $("button").click(function() {
+    $.ajax({
+      url: "https://dummyjson.com/todos",
+      method: "GET",
+      success: function(data) {
+        var todos = data.todos;
+        var todoList = $("ul");
+        todoList.empty();
+        todos.forEach(function(todo) {
+          todoList.append("<li>" + todo.todo + "</li>");
+        });
+      },
+      error: function() {
+        alert("Failed to load to-do items.");
+      }
+    });
+  });
+});
